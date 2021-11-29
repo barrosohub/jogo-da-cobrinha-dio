6f92848e6e0463af354a0837d6c0fcf63a6199cd
barrsofilho <barrosofilho.ti@gmail.com>
2021-11-28 21:43:15 -0300

    create food/target for snake attack

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ snake[0] = {
     y: 8 * box
 }
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 function direction() {
     if (direction == "right") {
         snake[0].x += box;
@@ -47,10 +57,10 @@ function iniciarJogo() {
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
-    
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
